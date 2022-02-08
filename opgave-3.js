17849cca2c95c3a54226869e80e468c030375125
cam-emmauscollege <cam@emmauscollege.nl>
2022-02-08 08:22:32 +0000

    werkende versie

---
+++ opgave-3.js
@@ -0,0 +1,13 @@
+module.exports = {grootsteGetal}
+/* Algoritmiek opgave #3
+   Opdracht: pas de functie zo aan,
+   dat de functie het grootste getal
+   van a en b teruggeeft
+*/
+
+function grootsteGetal(a, b) {
+  var uitkomst = a;
+  // vul hieronder aan
+  
+  return uitkomst;
+};
